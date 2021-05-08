f725582ea14e0fc25f2d3dbeae9aef6796a93b67
David Mateus <dmateusb@unal.edu.co>
2021-05-07 19:06:51 -0500

    Renaming variable periods

--- src/util/user_verification.js
+++ src/util/user_verification.js
@@ -1,9 +1,9 @@
 const perm = require('../models/permission.js');
 module.exports = async (user_id) => {
-  const periods = await perm.findPeriod(user_id)
+  const schedules = await perm.findPeriod(user_id)
   var found = false
-  periods.forEach(permission => {
-    let times = permission.period.split(",");
+  schedules.forEach(schedule => {
+    let times = schedule.period.split(",");
     let init = new Date(times[0].substr(1, times[0].length - 2)).valueOf();
     let end = new Date(times[1].substr(0, times[1].length - 2)).valueOf();
     let currentTime = new Date().valueOf() - 5 * 3600 * 1000;
