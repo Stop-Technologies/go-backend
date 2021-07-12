8dfa7b2af957f555b978cf913108542ae3b4eebd
david <dmateusb@unal.edu.co>
2021-07-12 00:14:19 -0500

    Add seeds

--- src/routes/admin/places.js
+++ src/routes/admin/places.js
@@ -2,6 +2,7 @@ const Router = require('express')
 const router = Router()
 const { authorize } = require('../../auth/service')
 const places = require('../../controllers/places')
+const { countCurrentGuests } = require('../../controllers/entries')
 
 router.use(authorize(['admin']))
 
@@ -58,6 +59,28 @@ router.delete('/:id', function (req, res) {
     .then((place) => {
       if (place) {
         res.send({ place: place, success: true })
+      } else {
+        res.status(500).send({ error: 'Place not found', success: false })
+      }
+    })
+    .catch(((error) => {
+      res.status(500).send({ error: error.message, success: false })
+    }))
+})
+
+
+router.get('/:id/current_guests', function (req, res) {
+  places.find(req.params.id)
+    .then((place) => {
+      if (place) {
+        countCurrentGuests(place.id)
+        .then((currentUsers) => {
+          if (currentUsers != false){
+            res.send({ currentUsers: currentUsers, success: true })
+          }else{
+            res.status(500).send({ error: 'Guests not found', success: false })
+          }
+        })
       } else {
         res.status(500).send({ error: 'Place not found', success: false })
       }
